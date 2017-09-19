22d36d558d5670ca8a35b01388b89882a1fc6a31
Chris Anstey <chris@cpanstey.co.uk>
2017-09-19 14:36:05 +0100

    Added callApiWithParams() function, and made callApi() func a wrapper of the new func. Added getBlock() call, which currently gets the latest block.

--- etherview.go
+++ etherview.go
@@ -16,9 +16,10 @@ type Request1 struct {
     Jsonrpc   string      `json:"jsonrpc"`
     Method   string `json:"method"`
     Id   int `json:"id"`
+    Params interface{} `json:"params"`
 }
 
-func callApi(method string) []byte {
+func callApiWithParams(method string, params interface{}) []byte {
 
     url := "http://192.168.1.145:8545"
     url = "http://127.0.0.1:8545"
@@ -27,6 +28,7 @@ func callApi(method string) []byte {
         Jsonrpc:   "2.0",
         Method: method,
         Id: 1,
+        Params: params,
     }
     queryJson, _ := json.Marshal(queryData)
     // fmt.Println(string(queryJson))
@@ -58,6 +60,12 @@ func callApi(method string) []byte {
 
 }
 
+func callApi(method string) []byte {
+
+    return callApiWithParams(method, [0]string{})
+
+}
+
 func blockNumber() string {
 
     body := callApi("eth_blockNumber")
@@ -73,6 +81,8 @@ func blockNumber() string {
     output += fmt.Sprintf("Res2:  %s!", dat)
     output += fmt.Sprintf("<br /><br />")
     output += fmt.Sprintf("Res3:  %s!", dat["result"])
+    output += fmt.Sprintf("<br /><br />")
+    output += fmt.Sprintf("<br /><br />")
 
     return output
 }
@@ -132,6 +142,67 @@ func syncing() string {
     return output
 
 }
+
+
+func getBlock() string {
+
+    body := callApiWithParams("eth_getBlockByNumber", []interface{}{"latest", true})
+
+    output := fmt.Sprintf("Res:  %s!", body)
+
+    var dat map[string]interface{}
+
+    if err := json.Unmarshal(body, &dat); err != nil {
+        panic(err)
+    }
+    output += fmt.Sprintf("<br /><br />")
+    output += fmt.Sprintf("Res2:  %s!", dat)
+    output += fmt.Sprintf("<br /><br />")
+    output += fmt.Sprintf("Res3:  %s!", dat["result"])
+
+
+    output += fmt.Sprintf("<br /><br />")
+    switch vv := dat["result"].(type) {
+	    case bool:
+		    output += fmt.Sprintf("Not syncing")
+	    case map[string]interface {}:
+	        output += fmt.Sprintf("Syncing")
+		    output += fmt.Sprintf("<table>")
+            for i, u := range vv {
+			    output += fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>", i, u)
+            }
+		    output += fmt.Sprintf("<table>")
+	    default:
+	        output += fmt.Sprintf("is of a type I don't know how to handle", reflect.TypeOf(vv))
+    }
+    output += fmt.Sprintf("<br /><br />")
+
+    // for k, v := range dat {
+    //     switch vv := v.(type) {
+    //     case string:
+    //         fmt.Println(k, "is string", vv)
+    //     case int:
+    //         fmt.Println(k, "is int", vv)
+    //     case float64:
+    //         fmt.Println(k, "is float64", vv)
+    //     case map[string]interface {}:
+		  //   output += fmt.Sprintf("<table>")
+    //         for i, u := range vv {
+			 //    output += fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>", i, u)
+    //         }
+		  //   output += fmt.Sprintf("<table>")
+    //     default:
+    //         fmt.Println(k, "is of a type I don't know how to handle", reflect.TypeOf(vv))
+    //     }
+    // }
+
+    return output
+
+}
+
+
+
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `<!doctype html>
 		<html lang="en">
@@ -144,6 +215,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
     body := syncing()
     body += blockNumber()
+    body += getBlock()
 
     fmt.Fprintf(w, "%s", body)
 
